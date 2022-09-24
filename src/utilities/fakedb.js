416dde76479123a825c7a77a14c4fdd46916e39f
RaisulislamRimon <rimonppip0@gmail.com>
2022-09-25 05:45:30 +0600

    this will give object object result in localStorage

--- src/utilities/fakedb.js
+++ src/utilities/fakedb.js
@@ -1,12 +1,31 @@
 const addToDb = (id) => {
-  const quantity = localStorage.getItem(id);
+  let shoppingCart = {};
+
+  // const quantity = localStorage.getItem(id);
+  const quantity = shoppingCart[id];
 
   if (quantity) {
-    const newQuantity = +quantity + 1;
-    localStorage.setItem(id, newQuantity);
+    const newQuantity = quantity + 1;
+    // localStorage.setItem(id, newQuantity);
+    shoppingCart[id] = newQuantity;
   } else {
-    localStorage.setItem(id, 1);
+    // localStorage.setItem(id, 1);
+    shoppingCart[id] = 1;
   }
+  localStorage.setItem("shopping-cart", shoppingCart);
 };
 
 export { addToDb };
+
+// const addToDb = (id) => {
+//   const quantity = localStorage.getItem(id);
+
+//   if (quantity) {
+//     const newQuantity = +quantity + 1;
+//     localStorage.setItem(id, newQuantity);
+//   } else {
+//     localStorage.setItem(id, 1);
+//   }
+// };
+
+// export { addToDb };
